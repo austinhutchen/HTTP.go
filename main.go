db59048c425c8e6d6d9d77dfeab6613ad5b38bed
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-04 18:06:47 -0800

    first

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
